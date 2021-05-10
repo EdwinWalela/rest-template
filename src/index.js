c64ece3de871129265f447400edcbbd3f76c7932
EdwinWalela <edwinwalela@gmail.com>
2021-05-10 10:10:10 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const DB_URI = process.env.DB_URI;
+
+app.use(cors());
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.status(200).send({
+        msg:"OK"
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log(`Listening for requests on port ${PORT}`);
+})
